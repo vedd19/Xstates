17ca0c3c86066d8314dace2ee5b9e284e4a119a6
vedd19 <ved123official@gmail.com>
2025-09-06 18:53:38 +0530

    first commit

---
+++ src/Xstates.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Xstates() {
+
+    const [countries, setCountries] = useState([]);
+    const [states, setStates] = useState([]);
+    const [cities, setCities] = useState([]);
+
+    const [selectedCountry, setSelectedCountry] = useState("");
+    const [selectedState, setSelectedState] = useState("");
+    const [selectedCity, setSelectedCity] = useState("");
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            setStates([]);
+            setCities([]);
+            setSelectedState("");
+            setSelectedCity("");
+            const countriesResponse = await fetch('https://crio-location-selector.onrender.com/countries');
+            const countriesData = await countriesResponse.json();
+            setCountries(countriesData);
+
+        };
+
+        fetchData();
+    }, [selectedCountry]);
+
+    useEffect(() => {
+        setCities([]);
+        setSelectedCity("");
+        console.log(selectedCountry, "selected country from useEffect");
+        fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`).then((res) => res.json()).then((data) => setStates(data));
+
+    }, [selectedCountry])
+
+    useEffect(() => {
+        console.log(selectedState, "selected state from useEffect");
+        fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`).then((res) => res.json()).then((data) => setCities(data));
+
+    }, [selectedState])
+
+
+
+
+    return (
+        <div>
+            <h1>Select Location</h1>
+            <div style={{ display: "flex", gap: "20px", justifyContent: "center", marginTop: "50px" }}>
+                <select style={{ padding: "4px" }} onChange={(e) => { setSelectedCountry(e.target.value) }}>
+                    <option >Select Country</option>
+
+                    {countries.map((country) => (
+                        <option key={country} value={country}>{country}</option>
+                    ))}
+                </select>
+
+                <select style={{ padding: "4px" }} disabled={!selectedCountry} onChange={(e) => { setSelectedState(e.target.value) }}>
+                    <option selected={true}>Select State</option>
+
+                    {selectedCountry && states.map((state) => (
+                        <option key={state} value={state}>{state}</option>
+                    ))}
+                </select>
+
+                <select style={{ padding: "4px" }} disabled={!selectedState} onChange={(e) => { setSelectedCity(e.target.value) }}>
+
+                    <option selected={true} >Select City</option>
+
+                    {selectedState && cities.map((city) => (
+                        <option key={city} value={city}>{city}</option>
+                    ))}
+                </select>
+
+
+
+            </div>
+
+            {selectedCity && <div style={{ display: 'flex', gap: '5px', alignItems: 'baseline', justifyContent: 'center' }}>
+                <h2>You Selected </h2> <h1>{selectedCountry},</h1> <h2 style={{ color: 'gray' }}>{selectedState} , {selectedCity}</h2>
+            </div>}
+
+        </div>
+
+    )
+}
